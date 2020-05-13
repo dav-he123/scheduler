69226b5cbae6655a55aa0cb53b969496862b45bc
David <david.he07@yahoo.ca>
2020-05-13 17:23:05 -0400

    updated code for API

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,36 @@ import "components/Application.scss";
 
 import DayList from "components/DayList";
 
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+
+  {
+    id: 4,
+    time: "3pm",
+  },
+];
+
 export default function Application(props) {
   const days = [
     {
@@ -25,6 +55,11 @@ export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
   console.log(day);
+
+  const scheduleList = appointments.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -46,6 +81,8 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {scheduleList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
