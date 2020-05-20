4afbaed3076cc6015dc64e93ecc3398ebcdaf9b5
David <david.he07@yahoo.ca>
2020-05-20 13:01:49 -0400

    included testing code to expecting Saving text to display

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -8,9 +8,12 @@ import {
   getByText,
   prettyDOM,
   getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
+import axios from "axios";
 
 afterEach(cleanup);
 
@@ -31,13 +34,25 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
 
-    const appointment = getAllByTestId(container, "appointment")[0];
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving.....")).toBeInTheDocument();
 
     // console.log(container);
     console.log(prettyDOM(container));
